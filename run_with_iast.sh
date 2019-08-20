ae797651c8cd60ec29fafcabd789c1ec10f5b1d7
tpappas <tpappas@veracode.com>
2019-08-20 17:30:23 -0400

    iterating..

--- run_with_iast.sh
+++ run_with_iast.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
-export MAVEN_OPTS=-agentlib:${PWD}/agent_nodejs_linux64
+MAVEN_OPTS=-agentlib:${PWD}/agent_nodejs_linux64
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH};${PWD}
 PATH=${PATH};${PWD}
-export IASTAGENT_LOGGING_STDERR_LEVEL=info
-export IASTAGENT_LOGGING_STDERR_ENABLED=true 
+IASTAGENT_LOGGING_STDERR_LEVEL=info
+IASTAGENT_LOGGING_STDERR_ENABLED=true 
 mvn --batch-mode test
