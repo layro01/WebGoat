88d5ebe20b66db08a034807fbbee7436ce30602d
Thomas <tpappas@veracode.com>
2019-09-12 16:05:48 -0700

    Lowered debugging level

--- run_with_iast.sh
+++ run_with_iast.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
+
+#curl -sSL https://s3.us-east-2.amazonaws.com/app.veracode-iast.io/java/agent-java-linux64.tar.gz -o java.zip
+#tar xvf java.zip
+
 export MAVEN_OPTS=-agentlib:agent_java_linux64
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}
 export PATH=${PATH}:${PWD}
-export IASTAGENT_LOGGING_STDERR_LEVEL=info
-export IASTAGENT_LOGGING_STDERR_ENABLED=true 
 mvn --batch-mode test
